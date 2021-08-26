be2370f9b0e77f50513c7c25b384e338826a8a35
evg1k80 <evg1k80@gmail.com>
2021-08-26 14:04:45 +0300

    clean js

--- #src/js/files/script.js
+++ #src/js/files/script.js
@@ -1,20 +1,6 @@
 window.onload = function() {
 
-    const headerElement = document.querySelector('.header');
     
-    const callback = function (entries, observer) {
-        if (entries[0].isIntersecting) {
-            headerElement.classList.remove('_scroll');
-            
-        } else {
-            headerElement.classList.add('_scroll');
-            
-        }
-    }
-
-    const headerObserver = new IntersectionObserver(callback);
-
-    headerObserver.observe(headerElement);
 
     const sidebarBtn = document.querySelector('.sidebar__btn');
     const sidebar = document.querySelector('.sidebar');
--- GrandTheatre_v1/js/app.js
+++ GrandTheatre_v1/js/app.js
@@ -264,21 +264,7 @@ function _is_hidden(el) {
 
 window.onload = function() {
 
-    const headerElement = document.querySelector('.header');
     
-    const callback = function (entries, observer) {
-        if (entries[0].isIntersecting) {
-            headerElement.classList.remove('_scroll');
-            
-        } else {
-            headerElement.classList.add('_scroll');
-            
-        }
-    }
-
-    const headerObserver = new IntersectionObserver(callback);
-
-    headerObserver.observe(headerElement);
 
     const sidebarBtn = document.querySelector('.sidebar__btn');
     const sidebar = document.querySelector('.sidebar');
